7116cafc0604f5baa828190c055a05384e4b2d4a
drillerguy <mikew@s-ug.net>
2026-09-10 16:34:09 -0500

    Cache ScanIn v0.2 location check-in files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "scanin-v0.1.3";
-const APP_SHELL = ["./","./index.html","./styles.css","./iphone-fix.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE_NAME = "scanin-v0.2.0";
+const APP_SHELL = ["./","./index.html","./styles.css","./iphone-fix.css","./location.css","./app.js","./location.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",(event)=>{event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",(event)=>{event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE_NAME).map((key)=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",(event)=>{if(event.request.method!=="GET")return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then((response)=>{const copy=response.clone();caches.open(CACHE_NAME).then((cache)=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then((cached)=>cached||caches.match("./index.html"))));});
